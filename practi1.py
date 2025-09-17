fa444d98f36d56bdde6e56132dc7cc1c63e68be1
Santiago-Lmd <20243RD048@utez.edu.mx>
2025-09-17 16:08:52 -0600

    Agregue un script de python

--- practi1.py
+++ practi1.py
@@ -8,6 +8,8 @@ git_dir = r"C:\Users\CC6\Desktop\practi-code"
 if not os.path.exist(git_dir):
     os.makedirs(git_dir)
     print("Created directory: {git_dir}")
+
 repos = [name for name in os.listdir(git_dir)
     if os.path.isdir(os.path.join(git_dir,name))and
     os.path.exists(os.path.join(git_dir, name, ".git"))]
+
